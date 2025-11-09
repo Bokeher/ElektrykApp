28fbb08b8c15b1e33775613e0c2a871bc4936c4e
Bokeher <bokeher47@gmail.com>
2025-11-09 08:31:29 +0100

    Dynamically fill replacement tab with data and remove placeholders

--- app/src/main/java/com/example/planlekcji/fragments/ui/ReplacementsFragment.java
+++ app/src/main/java/com/example/planlekcji/fragments/ui/ReplacementsFragment.java
@@ -2,22 +2,21 @@ package com.example.planlekcji.fragments.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.text.Html;
-import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.example.planlekcji.MainActivity;
 import com.example.planlekcji.MainViewModel;
 import com.example.planlekcji.R;
 import com.example.planlekcji.ckziu_elektryk.client.replacements.Replacement;
 import com.example.planlekcji.ckziu_elektryk.client.replacements.ReplacementChange;
-import com.example.planlekcji.ckziu_elektryk.client.timetable.SchoolEntryType;
 import com.example.planlekcji.replacements.ReplacementDataDownloader;
 
 import java.text.SimpleDateFormat;
@@ -26,17 +25,20 @@ import java.util.List;
 import java.util.Locale;
 
 public class ReplacementsFragment extends Fragment {
-    private View view;
     private List<List<Replacement>> replacements;
     private MainViewModel mainViewModel;
+    private LayoutInflater inflater;
+    private LinearLayout layout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_replacements, container, false);
-        mainViewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
+        View view = inflater.inflate(R.layout.fragment_replacements, container, false);
+        this.mainViewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
+        this.inflater = inflater;
+        this.layout = view.findViewById(R.id.linearLayout_replacements);
 
         observeAndHandleReplacementsLiveData();
-        mainViewModel.fetchReplacements();
+        this.mainViewModel.fetchReplacements();
 
         return view;
     }
@@ -45,22 +47,23 @@ public class ReplacementsFragment extends Fragment {
         mainViewModel.getReplacementsLiveData().observe(getViewLifecycleOwner(), newReplacements -> {
             replacements = newReplacements;
 
-            if (replacements == null || replacements.isEmpty()) return;
-
             updateReplacements();
         });
     }
 
     private void updateReplacements() {
-        TextView textFieldReplacements = view.findViewById(R.id.textView_replacements);
-        TextView textView_noResults = view.findViewById(R.id.textView_noResults);
+        layout.removeAllViews();
 
         if(replacements == null || replacements.isEmpty() || areReplacementsEmpty()) {
-            textFieldReplacements.setText(getString(R.string.no_replacements));
+            CardView dayCard = (CardView) inflater.inflate(R.layout.replacement_day_card, layout, false);
+            TextView dayTitle = dayCard.findViewById(R.id.textView_dayTitle);
+            dayTitle.setText(R.string.no_replacements);
+
+            layout.addView(dayCard);
+
             return;
         }
 
-        StringBuilder displayedText = new StringBuilder();
         Date[] dates = ReplacementDataDownloader.getNext5Dates(); // holds next 5 non-weekend dates
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd EEEE", Locale.getDefault());
 
@@ -68,40 +71,41 @@ public class ReplacementsFragment extends Fragment {
         for (List<Replacement> dayReplacements : replacements) {
             if(dayReplacements.isEmpty()) continue;
 
-            if(i != 0) {
-                displayedText.append("<br><br>");
-            }
+            CardView dayCard = (CardView) inflater.inflate(R.layout.replacement_day_card, layout, false);
+            LinearLayout dayCardLayout = dayCard.findViewById(R.id.replacementDay_layout);
 
-            displayedText.append("<h2>")
-                    .append(sdf.format(dates[i]))
-                    .append("</h2>");
+            TextView dayTitle = dayCard.findViewById(R.id.textView_dayTitle);
+            dayTitle.setText(sdf.format(dates[i]));
 
             for (Replacement replacement : dayReplacements) {
-                displayedText.append(replacementToString(replacement));
-                displayedText.append("<br>");
+                CardView replacementCard = (CardView) inflater.inflate(R.layout.replacement_card, dayCardLayout, false);
+                TextView replacementTitle = replacementCard.findViewById(R.id.textView_replacementTitle);
+                TextView replacementDetails = replacementCard.findViewById(R.id.textView_replacementDetails);
+
+                replacementTitle.setText(replacement.name());
+
+                StringBuilder details = getReplacementDetails(replacement);
+                replacementDetails.setText(details.toString());
+
+                dayCardLayout.addView(replacementCard);
             }
 
+            layout.addView(dayCard);
             i++;
         }
-
-        Spanned spannedText = Html.fromHtml(displayedText.toString(), Html.FROM_HTML_MODE_LEGACY);
-        textFieldReplacements.setText(spannedText);
-        textView_noResults.setVisibility(View.GONE);
     }
 
-    private String replacementToString(Replacement replacement) {
-        StringBuilder res = new StringBuilder();
-        SchoolEntryType timetableType = MainActivity.getTimetableType();
-        if(timetableType != SchoolEntryType.CLASSES) {
-            res.append(replacement.name()).append("<br>");
-        }
-
+    private static @NonNull StringBuilder getReplacementDetails(Replacement replacement) {
         List<ReplacementChange> changes = replacement.changes();
+        StringBuilder stringBuilder = new StringBuilder();
         for (ReplacementChange change : changes) {
-            res.append(change.period()).append(" | ").append(change.info()).append("<br>");
+            stringBuilder.append(change.period()).append(" | ").append(change.info()).append("\n");
         }
 
-        return res.toString();
+        if (stringBuilder.length() > 0 && stringBuilder.charAt(stringBuilder.length() - 1) == '\n') {
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        }
+        return stringBuilder;
     }
 
     private boolean areReplacementsEmpty() {
